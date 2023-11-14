510adcf46c38a7d1ea86ae5937f44f195f8a1859
Boltcrasher <Boltcrasher@gmail.com>
2023-11-14 13:29:45 -0600

    lab4

--- Lab4/Maze.java
+++ Lab4/Maze.java
@@ -1,7 +1,7 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
+//Name - Samuel Zhang
 
 import java.util.Scanner;
 import static java.lang.System.*;
@@ -16,16 +16,32 @@ public class Maze
 
 	public Maze(int[][] m)
 	{
+		maze = m;
 	}
 
 	public boolean checkForExitPath(int r, int c)
 	{
+		if (r >= 0 && r < maze.length && c >= 0 && c < maze[r].length && maze[r][c] == 1) {
+			maze[r][c] = 0;
+			if (c == maze[r].length - 1) {
+				return true;
+			}
+			else {
+				return checkForExitPath(r + 1, c) || checkForExitPath(r, c + 1) || checkForExitPath(r - 1, c) || checkForExitPath(r, c - 1);
+			}
+		}
 		return false;
 	}
 
 	public String toString()
 	{
 		String output="";
+		for (int r = 0; r < maze.length; r++) {
+			for (int c = 0; c < maze[r].length; c++) {
+				output += maze[r][c] + " ";
+			}
+			output += "\n";
+		}
 		return output;
 	}
 }
